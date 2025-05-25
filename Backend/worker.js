c5a6182b83e0cf83d18fc2cee998da8dfb44169d
0xarchit <0xarchit@gmail.com>
2025-05-25 19:10:59 +0530

    Fixed Download Error

--- Backend/worker.js
+++ Backend/worker.js
@@ -1,4 +1,4 @@
-const COOKIE = "<your_cookie>" // Replace with your actual cookie
+const COOKIE = "<cookie>" // Replace with your actual cookie
 
 const HEADERS = {
   "Accept": "application/json, text/plain, */*",
@@ -23,7 +23,7 @@ const DL_HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
   "Accept-Language": "en-US,en;q=0.5",
-  "Referer": "https://www.terabox.com/",
+  "Referer": "https://terabox.com/",
   "DNT": "1",
   "Connection": "keep-alive",
   "Upgrade-Insecure-Requests": "1",
@@ -93,7 +93,7 @@ async function getFileInfo(link, request) {
       root: "1,",
     });
 
-    response = await fetch(`https://www.terabox.app/share/list?${params}`, { headers: HEADERS });
+    response = await fetch(`https://dm.terabox.app/share/list?${params}`, { headers: HEADERS });
     const data = await response.json();
 
     if (!data || !data.list || !data.list.length || data.errno) {
